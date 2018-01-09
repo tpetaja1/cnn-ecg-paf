7f8aada67d6a2b61e765a8097104b0628ba83348
tpetaja1 <timo.petaja@hotmail.com>
2018-01-09 19:48:25 +0200

    small edits to main file
    -clarifications for variable names
    -adaptation to data handler and cnn changes

--- paf_classifier.py
+++ paf_classifier.py
@@ -16,30 +16,47 @@ class PAFClassifier():
                                         verbose=self.verbose)
         self.data_handler.load_training_data()
         self.data_handler.load_test_data()
-        self.data_length = 38400  # 5 min
-        self.number_of_minibatches = 6
-        self.model = CNN(number_of_filters=10, regularization_coefficient=0.1,
-                         learning_rate=0.1)
-        self.model.create_computational_graph()
+        self.sub_ecg_length = 38400  # 5 min
+        self.number_of_sub_ecgs = 6
+        self.mini_batch_size = 10
+        self.model = CNN(number_of_filters=16,
+                         regularization_coefficient=1e-1,
+                         learning_rate=0.01,
+                         filter_length=16,
+                         mini_batch_size=self.mini_batch_size,
+                         pool_size=2,
+                         fully_connected_layer_neurons=16,
+                         momentum=0.9)
         self.number_of_epochs = number_of_epochs
 
     def create_models(self):
+
         if self.verbose:
             print("Creating Training model...")
+
+        x = T.dmatrix('x')
+        y = T.ivector('y')
+
+        self.model.create_computational_graph(x, y)
+
         set_index = T.lscalar()
         index = T.lscalar()
+
         self.train_model = \
             theano.function(
                 [set_index, index],
                 (self.model.cost, self.model.error),
                 updates=self.model.updates,
-                givens={self.model.x:
+                givens={x:
                         self.data_handler.training_data[
-                            set_index,
-                            index * self.data_length:
-                            (index + 1) * self.data_length],
-                        self.model.y:
-                        self.data_handler.training_labels[set_index]
+                            set_index * self.mini_batch_size:
+                            (set_index + 1) * self.mini_batch_size,
+                            index * self.sub_ecg_length:
+                            (index + 1) * self.sub_ecg_length],
+                        y:
+                        self.data_handler.training_labels[
+                            set_index * self.mini_batch_size:
+                            (set_index + 1) * self.mini_batch_size]
                         }
             )
         if self.verbose:
@@ -51,21 +68,26 @@ class PAFClassifier():
             theano.function(
                 [set_index, index],
                 (self.model.cost, self.model.error),
-                givens={self.model.x:
+                givens={x:
                         self.data_handler.test_data[
-                            set_index,
-                            index * self.data_length:
-                            (index + 1) * self.data_length],
-                        self.model.y:
-                        self.data_handler.test_labels[set_index]
+                            set_index * self.mini_batch_size:
+                            (set_index + 1) * self.mini_batch_size,
+                            index * self.sub_ecg_length:
+                            (index + 1) * self.sub_ecg_length],
+                        y:
+                        self.data_handler.test_labels[
+                            set_index * self.mini_batch_size:
+                            (set_index + 1) * self.mini_batch_size]
                         }
             )
         if self.verbose:
             print("Test model created.")
 
     def train_neural_network(self):
+
         if self.verbose:
             print("Training the model...")
+
         for epoch_index in range(1, self.number_of_epochs + 1):
 
             if self.verbose:
@@ -75,43 +97,48 @@ class PAFClassifier():
             """ Train model """
             train_errors = 0
             for training_set_index in range(
-                    self.data_handler.number_of_training_sets):
-                for minibatch_index in range(self.number_of_minibatches):
-                    train_cost, train_error = self.train_model(
-                        training_set_index, minibatch_index)
+                    self.data_handler.number_of_training_sets //
+                    self.mini_batch_size):
+                for minibatch_index in range(self.number_of_sub_ecgs):
+                    train_cost, train_error = \
+                        self.train_model(training_set_index, minibatch_index)
                     train_errors += train_error
+
             if self.verbose:
                 print("    Train Errors: {}/{}, {:.2f}%".
                       format(train_errors,
                              self.data_handler.number_of_training_sets *
-                             self.number_of_minibatches,
+                             self.number_of_sub_ecgs,
                              100 * train_errors /
                              (self.data_handler.number_of_training_sets *
-                              self.number_of_minibatches)))
+                              self.number_of_sub_ecgs)))
                 print("    Train Cost: {}".format(train_cost))
 
             """ Test model """
             test_errors = 0
             for test_set_index in range(
-                    self.data_handler.number_of_test_sets):
-                for minibatch_index in range(self.number_of_minibatches):
-                    test_cost, test_error = self.test_model(
-                        test_set_index, minibatch_index)
+                    self.data_handler.number_of_test_sets //
+                    self.mini_batch_size):
+                for minibatch_index in range(self.number_of_sub_ecgs):
+                    test_cost, test_error = \
+                        self.test_model(test_set_index, minibatch_index)
                     test_errors += test_error
+
             if self.verbose:
                 print("    Test Errors: {}/{}, {:.2f}%".
                       format(test_errors,
                              self.data_handler.number_of_test_sets *
-                             self.number_of_minibatches,
+                             self.number_of_sub_ecgs,
                              100 * test_errors /
                              (self.data_handler.number_of_test_sets *
-                              self.number_of_minibatches)))
+                              self.number_of_sub_ecgs)))
                 print("    Test Cost: {}".format(test_cost))
+
         if self.verbose:
             print("Model trained.")
 
 
 if __name__ == "__main__":
-    p = PAFClassifier(number_of_epochs=3)
+    p = PAFClassifier(number_of_epochs=50)
     p.create_models()
     p.train_neural_network()

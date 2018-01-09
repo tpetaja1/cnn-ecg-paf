7816b53daf5b59e6eb5aa9e3c3d5812dba44e754
tpetaja1 <timo.petaja@hotmail.com>
2018-01-09 19:44:58 +0200

    developed CNN
    -more parameters
    -convolution uses 1D conv
    -option for normalization of data
    -parameter update into momentum method
    -separate fully connected layer

--- cnn.py
+++ cnn.py
@@ -2,127 +2,188 @@
 import numpy as np
 import theano
 import theano.tensor as T
-from theano.tensor.nnet import conv2d
+
+from theano.tensor.nnet.bn import batch_normalization_train
+from theano.tensor.nnet.abstract_conv import causal_conv1d
 from theano.tensor.signal import pool
 
 
 class CNN():
 
-    def __init__(self, learning_rate=0.01,
+    def __init__(self,
+                 learning_rate=0.01,
                  data_length_in_mini_batch=38400,
+                 mini_batch_size=10,
                  number_of_filters=1,
                  filter_length=15,
                  activation=T.tanh,
                  classification_method=T.nnet.softmax,
                  pool_mode="max",
-                 regularization_coefficient=0.001):
+                 regularization_coefficient=0.001,
+                 fully_connected_layer_neurons=16,
+                 pool_size=2,
+                 momentum=0.9,
+                 perform_normalization=True):
 
         """ Initialize hyperparameters """
+        self.learning_rate = learning_rate
         self.data_length_in_mini_batch = data_length_in_mini_batch
+        self.mini_batch_size = mini_batch_size
         self.number_of_filters = number_of_filters
         self.filter_length = filter_length
-        self.activation_in_conv_layer = activation
-        self.learning_rate = learning_rate
-        self.pool_mode = "max"
+        self.activation = activation
         self.classification_method = classification_method
+        self.pool_mode = pool_mode
         self.regularization_coefficient = regularization_coefficient
+        self.momentum = momentum
+        self.perform_normalization = perform_normalization
 
         """ Initialize shapes """
         self.convolution_input_shape = \
-            (1, 1, self.data_length_in_mini_batch, 1)
-        self.filter_shape = (self.number_of_filters, 1, self.filter_length, 1)
-        self.pool_shape = (2, 1)
+            (self.mini_batch_size, 1, self.data_length_in_mini_batch)
+
+        self.filter_shape = (self.number_of_filters, 1, self.filter_length)
+
+        self.pool_shape = (1, pool_size)
+
         self.fully_connected_layer_shape = \
-            (int(self.number_of_filters*(
-                self.data_length_in_mini_batch + self.filter_length - 1) /
-             self.pool_shape[0]),
-             2)
+            (self.number_of_filters*(
+                int(self.data_length_in_mini_batch /
+                    self.pool_shape[1])),
+             fully_connected_layer_neurons)
+
+        self.output_layer_shape = (fully_connected_layer_neurons, 2)
 
         """ Initialize parameters """
-        self.filter_weights = None
-        self.bias_convolution = None
-        self.W_fully_connected = None
-        self.bias_fully_connected = None
+        self.parameters = []
+        self.updates = []
 
-    def create_computational_graph(self):
+    def create_computational_graph(self, x, y):
 
-        # Symbolic Theano variables
-        self.x = T.dvector('x')
-        self.y = T.iscalar('y')
+        # Shape: 10  x  1  x  length_of_data
+        input_data = self.input_layer(x)
 
-        # Shape: 1  x  1  x  length_of_data  x  1
-        input_data = self.input_layer(self.x)
-
-        # Shape: 1  x  number_of_filters  x
-        #        (length_of_data + length_of_filter - 1)  x  1
+        # Shape: 10  x  number_of_filters  x
+        #        length_of_data
         convolution_layer_output = self.convolution_layer(input_data)
 
-        # Shape: 1  x  number_of_filters  x
-        #        (convolution_output / subsampling_coefficient)  x  1
+        # Shape: 10  x  number_of_filters  x
+        #        (convolution_output / subsampling_coefficient)
         subsampling_layer_output = \
             self.subsampling_layer(convolution_layer_output)
 
-        # Shape: 1  x  (number_of_filters * subsampling_output)
+        # Shape: 10  x  (number_of_filters * subsampling_output)
         fully_connected_layer_input = subsampling_layer_output.flatten(2)
 
-        # Shape: 1  x  2
+        # Shape: 10  x  64
         fully_connected_layer_output = \
             self.fully_connected_layer(fully_connected_layer_input)
 
-        # Shape: 1  x  2
-        probabilities = self.output_layer(fully_connected_layer_output)
+        # Shape: 10  x  2
+        self.probabilities = self.output_layer(fully_connected_layer_output)
 
-        prediction = T.argmax(probabilities, axis=1)
+        """ Predictions: Transformed into {-1, 1} """
+        self.prediction = T.sgn(T.argmax(self.probabilities, axis=1) - 0.5)
 
         regularizer = \
-            T.sqr(self.filter_weights.norm(L=2)) +\
-            T.sqr(self.W_fully_connected.norm(L=2))
+            T.sum(self.parameters[0] ** 2) +\
+            T.sum(self.parameters[2] ** 2) +\
+            T.sum(self.parameters[4] ** 2)
 
-        self.cost = \
-            -T.mean(T.log(probabilities)[0, self.y]) +\
-            self.regularization_coefficient * regularizer
+        negative_log_likelihood = \
+            -T.sum(T.log(self.probabilities)[T.arange(y.shape[0]),
+                                             T.cast(T.eq(y, 1), "int16")])
 
-        self.error = T.sum(T.neq(prediction, self.y))
+        penalty = self.regularization_coefficient * regularizer
 
-        self.parameters = [self.filter_weights,
-                           self.bias_convolution,
-                           self.W_fully_connected,
-                           self.bias_fully_connected]
+        self.cost = negative_log_likelihood + penalty
 
-        self.gradients = T.grad(self.cost, self.parameters)
+        self.error = T.sum(T.neq(self.prediction, y))
 
-        self.updates = \
-            [(parameter, parameter - self.learning_rate * gradient)
-             for parameter, gradient in zip(self.parameters, self.gradients)]
+        self.parameter_updates()
 
     def input_layer(self, input_data):
 
-        return input_data.reshape(self.convolution_input_shape)
+        if self.perform_normalization:
+            gamma = theano.shared(1.)
+            bias = theano.shared(0.)
+            running_mean = theano.shared(0.)
+            running_var = theano.shared(0.)
+
+            normalized_input_data, _, _,\
+                new_running_mean, new_running_var = \
+                batch_normalization_train(input_data,
+                                          gamma,
+                                          bias,
+                                          axes=(0, 1),
+                                          running_mean=running_mean,
+                                          running_var=running_var)
+
+            output = \
+                normalized_input_data.reshape(self.convolution_input_shape)
+
+            self.updates.append((running_mean, new_running_mean))
+            self.updates.append((running_var, new_running_var))
+
+        else:
+            output = input_data.reshape(self.convolution_input_shape)
+
+        return output
 
     def convolution_layer(self, input_data):
 
-        self.filter_weights = \
+        """ Weight and bias for the layer """
+        filter_weights = \
             theano.shared(np.asarray(
                 np.random.normal(0,
-                                 0.1,
+                                 1/np.sqrt(self.filter_shape[2]),
                                  size=self.filter_shape)),
                           name="Filter weights",
                           borrow=True)
 
-        self.bias_convolution = \
+        bias_convolution = \
             theano.shared(np.zeros((self.number_of_filters,),
                                    dtype=theano.config.floatX),
                           borrow=True)
 
-        output = conv2d(input=input_data,
-                        filters=self.filter_weights,
-                        filter_shape=self.filter_shape,
-                        input_shape=self.convolution_input_shape,
-                        border_mode="full")
+        """ Convolution """
+        convolution = causal_conv1d(input=input_data,
+                                    filters=filter_weights,
+                                    filter_shape=self.filter_shape,
+                                    input_shape=self.convolution_input_shape)
 
-        activation_output = \
-            self.activation_in_conv_layer(
-                output + self.bias_convolution.dimshuffle("x", 0, "x", "x"))
+        convolution_output = \
+            convolution + bias_convolution.dimshuffle("x", 0, "x")
+
+        if self.perform_normalization:
+            gamma = theano.shared(1.)
+            bias = theano.shared(0.)
+            running_mean = theano.shared(0.)
+            running_var = theano.shared(0.)
+
+            normalized_output, _, _,\
+                new_running_mean, new_running_var = \
+                batch_normalization_train(convolution_output,
+                                          gamma,
+                                          bias,
+                                          axes=(0, 1, 2),
+                                          running_mean=running_mean,
+                                          running_var=running_var)
+
+            self.updates.append((running_mean, new_running_mean))
+            self.updates.append((running_var, new_running_var))
+
+            activation_output = \
+                self.activation(normalized_output)
+
+        else:
+            activation_output = \
+                self.activation(
+                    convolution_output)
+
+        """ Add parameters to be updated """
+        self.parameters.append(filter_weights)
+        self.parameters.append(bias_convolution)
 
         return activation_output
 
@@ -130,33 +191,122 @@ class CNN():
 
         pool_out = pool.pool_2d(input=input_data,
                                 ws=self.pool_shape,
-                                ignore_border=True,
+                                ignore_border=False,
                                 mode=self.pool_mode)
 
         return pool_out
 
     def fully_connected_layer(self, input_data):
 
-        self.W_fully_connected = \
+        """ Weight and bias for the layer """
+        W_fully_connected = \
             theano.shared(np.asarray(
                 np.random.normal(0,
-                                 0.1,
+                                 1/np.sqrt(self.convolution_input_shape[0]),
                                  size=self.fully_connected_layer_shape)),
                           name="W fully connected",
                           borrow=True)
 
-        self.bias_fully_connected = \
+        bias_fully_connected = \
             theano.shared(np.zeros((self.fully_connected_layer_shape[1],),
                                    dtype=theano.config.floatX),
                           name="bias fully connected",
                           borrow=True)
 
-        output_fully_connected = \
-            T.dot(input_data, self.W_fully_connected) + \
-            self.bias_fully_connected
+        dot_output = \
+            T.dot(input_data, W_fully_connected) + bias_fully_connected
+
+        if self.perform_normalization:
+            gamma = theano.shared(1.)
+            bias = theano.shared(0.)
+            running_mean = theano.shared(0.)
+            running_var = theano.shared(0.)
+
+            normalized_output, _, _,\
+                new_running_mean, new_running_var = \
+                batch_normalization_train(dot_output,
+                                          gamma,
+                                          bias,
+                                          axes=(0, 1),
+                                          running_mean=running_mean,
+                                          running_var=running_var)
+
+            self.updates.append((running_mean, new_running_mean))
+            self.updates.append((running_var, new_running_var))
+
+            output_fully_connected = self.activation(
+                normalized_output)
+
+        else:
+            output_fully_connected = self.activation(dot_output)
+
+        """ Add parameters to be updated """
+        self.parameters.append(W_fully_connected)
+        self.parameters.append(bias_fully_connected)
 
         return output_fully_connected
 
     def output_layer(self, input_data):
-        output = self.classification_method(input_data)
+
+        """ Weight and bias for the layer """
+        W_output_layer = \
+            theano.shared(np.asarray(
+                np.random.normal(0,
+                                 1/np.sqrt(
+                                     self.fully_connected_layer_shape[1]),
+                                 size=self.output_layer_shape)),
+                          name="W output",
+                          borrow=True)
+
+        bias_output_layer = \
+            theano.shared(np.zeros((self.output_layer_shape[1],),
+                                   dtype=theano.config.floatX),
+                          name="bias output",
+                          borrow=True)
+
+        dot_output = T.dot(input_data, W_output_layer) + bias_output_layer
+
+        if self.perform_normalization:
+            gamma = theano.shared(1.)
+            bias = theano.shared(0.)
+            running_mean = theano.shared(0.)
+            running_var = theano.shared(0.)
+
+            normalized_output, _, _,\
+                new_running_mean, new_running_var = \
+                batch_normalization_train(dot_output,
+                                          gamma,
+                                          bias,
+                                          axes=(0, 1),
+                                          running_mean=running_mean,
+                                          running_var=running_var)
+
+            self.updates.append((running_mean, new_running_mean))
+            self.updates.append((running_var, new_running_var))
+
+            output = self.classification_method(normalized_output)
+
+        else:
+            output = self.classification_method(dot_output)
+
+        """ Add parameters to be updated """
+        self.parameters.append(W_output_layer)
+        self.parameters.append(bias_output_layer)
+
         return output
+
+    def parameter_updates(self):
+
+        self.gradients = T.grad(self.cost, self.parameters)
+
+        """ Momentum gradient update """
+        for parameter, gradient in zip(self.parameters, self.gradients):
+            velocity = theano.shared(parameter.get_value(borrow=True)*0.)
+
+            new_velocity = \
+                self.momentum * velocity - self.learning_rate * gradient
+            self.updates.append(
+                (velocity, new_velocity))
+            self.updates.append(
+                (parameter,
+                 parameter + new_velocity))
